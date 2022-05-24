d4c6978578de7d8c30c1f4728f9532d19e6de75d
TiagoCornalo <102361861+TiagoCornalo@users.noreply.github.com>
2022-05-24 17:35:46 -0300

    Update Maps.jsx

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -2,20 +2,30 @@ import React, { useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import './styles/Card.css'
 
+
 const BaseMap = (props) => {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiYmFrZXIxNiIsImEiOiJjbDNmdTR3YjEwcnVsM2pwNndmMXJzY2hmIn0.FaPrFwjyR0DFWohS-ntc2g'
+  // mapboxgl.accessToken = 'pk.eyJ1IjoiYmFrZXIxNiIsImEiOiJjbDNmdTR3YjEwcnVsM2pwNndmMXJzY2hmIn0.FaPrFwjyR0DFWohS-ntc2g'
 
+  // useEffect(() => {
+  //   new mapboxgl.Map({
+  //     container: "mapContainer",
+  //     style: "mapbox://styles/mapbox/streets-v11",
+  //     center: [props.lng, props.lat],
+  //     zoom: 12,
+  //   });
+  // });
 
-  useEffect(() => {
-    new mapboxgl.Map({
-      container: "mapContainer",
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [props.lng, props.lat],
-      zoom: 12,
-    });
-  });
+  return (<div id="mapContainer" className="mapDiv">
+    <iframe 
+     width='1000'
+     height= '400'
+    scrolling= 'yes'
+    marginHeight= '0'
+    marginWidth = '0'
+    src={`https://maps.google.com/maps?q= + ${props.lat} +, +${props.lng} +  &t=&z=13&ie=UTF8&iwloc=&output=embed`}>
 
-  return <div id="mapContainer" className="mapDiv"></div>;
+    </iframe>
+  </div>);
 };
 
 export default BaseMap;
